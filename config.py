63b8049206f8b5ad8fff5ab8a131d64a141bca9b
drekain <drekainsmith@gmail.com>
2026-01-28 11:27:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ class Config:
     API_ID = int(environ.get("API_ID", ""))
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "SteveBotz")
+    BOT_SESSION = environ.get("BOT_SESSION", "AutifrwdBot")
     DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "6317211079"))
+    DATABASE_NAME = environ.get("DATABASE_NAME", "sbfrwdbot")
+    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
     
 class temp(object): 
     lock = {}
@@ -15,4 +15,5 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
+
 
